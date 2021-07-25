a4c0c217a9932828fa7c9f5937b8542a45200f44
Koral <62809012+KoralK5@users.noreply.github.com>
2021-07-25 04:07:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def optimize(f, x, rate=0.1): return x - (f(x + 0.001) - f(x)) / 0.001 * rate
 def f(x): global y; return eval(y)
 x = float(input('x = ')); y = input('y = ')
-for iter in range(100):
-	x = optimize(f, x); print('\nx =', x, '\ny =', f(x))
+for iter in range(100): x = optimize(f, x); print('\nx =', x, '\ny =', f(x))
